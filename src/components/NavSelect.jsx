3c2bb080ff544551a490dd056855bb85b65e4e5d
Trinidad Sabina Lari <trinidadlari@gmail.com>
2024-06-24 01:32:52 -0300

    structure develop

--- src/components/NavSelect.jsx
+++ src/components/NavSelect.jsx
@@ -1,43 +1,11 @@
 import * as React from 'react';
-// import { useTheme } from '@mui/material/styles';
-// import OutlinedInput from '@mui/material/OutlinedInput';
-// import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
+
 import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-
-// const ITEM_HEIGHT = 48;
-// const ITEM_PADDING_TOP = 8;
-// const MenuProps = {
-//   PaperProps: {
-//     style: {
-//       maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-//       width: 250,
-//     },
-//   },
-// };
-
-// const names = [
-//   'Oliver Hansen',
-//   'Van Henry',
-//   'April Tucker',
-//   'Ralph Hubbard',
-//   'Omar Alexander',
-//   'Carlos Abbott',
-//   'Miriam Wagner',
-//   'Bradley Wilkerson',
-//   'Virginia Andrews',
-//   'Kelly Snyder',
-// ];
-
-//gggggg
-
-
-
 import FormHelperText from '@mui/material/FormHelperText';
-
-import { Box } from '@mui/material';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
 import TextField from '@mui/material/TextField';
+import { Box } from '@mui/material';
 
 
 export const NavSelect = (darkMode) => {
@@ -84,30 +52,6 @@ export const NavSelect = (darkMode) => {
             <FormHelperText>Listas de tareas</FormHelperText>
           </FormControl>
         </div>
-        {/* <div>
-          <FormControl sx={{ m: 1, width: 300 }}>
-            <InputLabel id="demo-multiple-name-label">Name</InputLabel>
-            <Select
-              labelId="demo-multiple-name-label"
-              id="demo-multiple-name"
-              multiple
-              value={personName}
-              onChange={handleChange}
-              input={<OutlinedInput label="Name" />}
-              MenuProps={MenuProps}
-            >
-              {names.map((name) => (
-                <MenuItem
-                  key={name}
-                  value={name}
-                  style={getStyles(name, personName, theme)}
-                >
-                  {name}
-                </MenuItem>
-              ))}
-            </Select> */}
-        {/* </FormControl>
-    </div > */}
       </Box >
     </>
   );
