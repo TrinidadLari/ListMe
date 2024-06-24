3eb7e7628b004539bc89a6edacc9b01de530a1d5
Trinidad Sabina Lari <trinidadlari@gmail.com>
2024-06-24 03:29:24 -0300

    functionality to add new task

--- src/components/NavSelect.jsx
+++ src/components/NavSelect.jsx
@@ -1,51 +1,43 @@
-import * as React from 'react';
-// import { useTheme } from '@mui/material/styles';
-// import OutlinedInput from '@mui/material/OutlinedInput';
-// import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
+/* eslint-disable react/prop-types */
+import { useState } from 'react';
+
 import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-
-// const ITEM_HEIGHT = 48;
-// const ITEM_PADDING_TOP = 8;
-// const MenuProps = {
-//   PaperProps: {
-//     style: {
-//       maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-//       width: 250,
-//     },
-//   },
-// };
-
-// const names = [
-//   'Oliver Hansen',
-//   'Van Henry',
-//   'April Tucker',
-//   'Ralph Hubbard',
-//   'Omar Alexander',
-//   'Carlos Abbott',
-//   'Miriam Wagner',
-//   'Bradley Wilkerson',
-//   'Virginia Andrews',
-//   'Kelly Snyder',
-// ];
-
-//gggggg
-
-
-
 import FormHelperText from '@mui/material/FormHelperText';
-
-import { Box } from '@mui/material';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
 import TextField from '@mui/material/TextField';
+import { Box } from '@mui/material';
+
+import { v4 as uuidv4 } from 'uuid';
 
 
-export const NavSelect = (darkMode) => {
-  const [state, setState] = React.useState('Todas');
+export const NavSelect = ({ darkMode, onAddTask }) => {
+  const [state, setState] = useState('Todas');
+  const [description, setDescription] = useState("");
 
-  const handleChange = (event) => {
-    setState(event.target.value);
+  const handleChange = (e) => {
+    setState(e.target.value);
   };
+
+  const handleDescriptionChange = (e) => {
+    setDescription(e.target.value);
+  };
+
+  const handleAddTask = () => {
+    if (description.trim() !== "") {
+      onAddTask({ description, checked: false, id: uuidv4() });
+      setDescription("");
+    } else {
+      alert("La tarea no puede estar vacía");
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleAddTask();
+    }
+  };
+
 
   return (
     <>
@@ -67,47 +59,23 @@ export const NavSelect = (darkMode) => {
           mt: 4
         }}
       >
-        <TextField id="outlined-basic" label="Agregar tarea" variant="outlined" sx={{ m: 1, width: '100%' }} />
-        <div>
+        <TextField id="outlined-basic" label="Agregar tarea" variant="outlined" sx={{ m: 1, width: '100%' }} value={description} onChange={handleDescriptionChange} onKeyDown={handleKeyDown}
+        />
 
-          <FormControl sx={{ m: 1, width: '100%' }}>
-            <Select
-              value={state}
-              onChange={handleChange}
-              inputProps={{ 'aria-label': 'Without label' }}
-              fullWidth
-            >
-              <MenuItem value={'Todas'} fullWidth>Todas</MenuItem>
-              <MenuItem value={'Realizadas'} fullWidth>Realizadas</MenuItem>
-              <MenuItem value={'Pendientes'} fullWidth>Pendientes</MenuItem>
-            </Select>
-            <FormHelperText>Listas de tareas</FormHelperText>
-          </FormControl>
-        </div>
-        {/* <div>
-          <FormControl sx={{ m: 1, width: 300 }}>
-            <InputLabel id="demo-multiple-name-label">Name</InputLabel>
-            <Select
-              labelId="demo-multiple-name-label"
-              id="demo-multiple-name"
-              multiple
-              value={personName}
-              onChange={handleChange}
-              input={<OutlinedInput label="Name" />}
-              MenuProps={MenuProps}
-            >
-              {names.map((name) => (
-                <MenuItem
-                  key={name}
-                  value={name}
-                  style={getStyles(name, personName, theme)}
-                >
-                  {name}
-                </MenuItem>
-              ))}
-            </Select> */}
-        {/* </FormControl>
-    </div > */}
+        <FormControl sx={{ m: 1, width: '50%' }}>
+          <Select
+            value={state}
+            onChange={handleChange}
+            inputProps={{ 'aria-label': 'Without label' }}
+            fullWidth
+          >
+            <MenuItem value={'Todas'}>Todas</MenuItem>
+            <MenuItem value={'Realizadas'} >Realizadas</MenuItem>
+            <MenuItem value={'Pendientes'} >Pendientes</MenuItem>
+          </Select>
+          <FormHelperText>Filtro de tareas</FormHelperText>
+        </FormControl>
+
       </Box >
     </>
   );
